c00603f252bb973b3e6e1075eb5dc04bfb32b421
nluomala <a1700370@myy.haaga-helia.fi>
2018-05-09 11:49:05 +0000

    Update2

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 setxkbmap fi
 sudo apt-get update
 salt-call --local state.highstate --file-root srv/salt
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
+sudo EXTERNAL_URL="http://gitlab.example.com" apt-get install gitlab-ee
---
+++ srv/salt/curl/init.sls
@@ -0,0 +1,4 @@
+curl:
+  run:
+    - curl: curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
+ 
